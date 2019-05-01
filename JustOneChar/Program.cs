4fc667bc142095588883751e1055be3616c6ba13
Robert Smidt <rsmidt909@hotmail.com>
2019-04-30 19:26:08 -0500

    creating repo

---
+++ JustOneChar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SpecChar Single = new SpecChar();
+            int Ind = Single.CodeChar();
+            Console.WriteLine(Ind);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ JustOneChar/SpecChar.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverseString
+{
+    class SpecChar
+    {
+        //Given the string “devCodeCamp”, return what index the character ‘m’ is at within the string.
+
+
+
+
+        //member variables (HAS A)
+        string input;
+        
+
+
+
+        //Constructor (BUILDS OBJECT)
+        public SpecChar()
+        {
+          input = "devCodeCamp";
+            
+        }
+
+
+
+
+
+        //member methods(CAN DO)
+        
+        public int CodeChar()
+        {
+            int letter = input.IndexOf("m");
+            return letter;
+        }
+        
+      
+     
+
+
+
+    }
+}
